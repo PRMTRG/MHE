97eda1b4b899b87e11d29dddd587c818c263a68a
TM <mirski.tymoteusz@gmail.com>
2020-10-25 16:21:15 +0100

    Lab 2

---
+++ main.py
@@ -0,0 +1,7 @@
+def rate_solution(guesses_list):
+    
+    return 0
+
+# guesses_list example:
+# guesses_list = (1234, 5678, 9012, 9087, 1087, 9205, 9305)
+# The last number in the list is the number that had to be guessed.
